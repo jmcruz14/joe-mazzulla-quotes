ecd52a590fcd69ded7868437b4bb84c1dc3fccaa
Jay <jccruz009@yahoo.com>
2024-09-13 00:07:33 +0800

    style: change to celtics theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,35 @@ export default {
     extend: {},
     fontFamily: {
       'body': ['Inter']
-    }
+    },
+    colors: {
+      celtics: {
+        green: '#007A33', // Celtics Green
+        white: '#FFFFFF', // White
+        gold: '#BA9653',  // Celtics Gold
+        black: '#000000', // Black
+      },
+    },
   },
   plugins: [
+    // require('@tailwindcss/typography'), 
     daisyui,
-    "@tailwindcss/typography"
   ],
   daisyui: {
-    themes: ["dark", "light", "forest"]
+    themes: ["dark", "light", "forest", {
+        celtics: {
+          primary: '#007A33', // Celtics Green
+          secondary: '#FFFFFF', // White
+          accent: '#BA9653', // Celtics Gold
+          neutral: '#000000', // Black
+          'base-100': '#FFFFFF', // Background white
+          info: '#A6D3A0', // Light Green (info color variation)
+          success: '#007A33', // Same Celtics Green for success
+          warning: '#F2C94C', // Light Gold/Yellow for warning
+          error: '#E74C3C', // Red for errors
+        },
+      },
+    ]
   }
 }
 
