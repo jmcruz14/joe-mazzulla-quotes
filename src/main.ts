885512161351313dca09118b0508025dc850f8df
Jay <jccruz009@yahoo.com>
2024-09-13 01:05:57 +0800

    feat: add analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { inject } from '@vercel/analytics'
 
-createApp(App).mount('#app')
+inject();
+createApp(App).mount('#app');
